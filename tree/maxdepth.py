1247d9d558b6f54f6618dcaf39704d8896ce9f6a
joonhyeok <joonhyeok.ahn3@wework.com>
2023-01-22 15:50:05 -0600

    Update the max depth of tree

--- tree/maxdepth.py
+++ tree/maxdepth.py
@@ -1,9 +1,4 @@
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
+from tree_node import TreeNode
 
 root = TreeNode(1)
 root.left = TreeNode(2)
