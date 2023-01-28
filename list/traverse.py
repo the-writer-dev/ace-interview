cc7cdf78d136f963f30c93b9d89f2d269a3868db
joonhyeok <joonhyeok.ahn3@wework.com>
2023-01-28 15:09:46 -0600

    Update the basics of linked list

---
+++ list/traverse.py
@@ -0,0 +1,32 @@
+from node import Node
+
+
+def traverse(head):
+    curr = head
+    while curr:
+        print(curr.data)
+        curr = curr.next
+
+
+def insert(head, data, pos):
+    new = Node(data)
+    if pos == 0:
+        new.next = head
+        head = new
+        return
+
+    prev = head
+    for _ in range(pos-1):
+        prev = prev.next
+    new.next = prev.next
+    prev.next = new
+
+
+def delete(head, pos):
+    if pos == 0:
+        head = head.next
+        return
+    prev = head
+    for _ in range(pos-1):
+        prev = prev.next
+    prev.next = prev.next.next
