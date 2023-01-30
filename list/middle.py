c93efb96c8f24e96ea6b6655702eb33473ccc0f8
joonhyeok <joonhyeok.ahn3@wework.com>
2023-01-30 06:25:39 -0600

    Update finding the mid node of the linked list

---
+++ list/middle.py
@@ -0,0 +1,10 @@
+
+# find the middle node of the given linked list
+
+def middle(head):
+    slow = fast = head
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+
+    return slow
