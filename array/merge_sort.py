f8522c11a769dac79c5b800168bcbf49e116fbba
joonhyeok <joonhyeok.ahn3@wework.com>
2023-02-10 12:40:46 -0600

    Update the merge sort on array

---
+++ array/merge_sort.py
@@ -0,0 +1,27 @@
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        l = arr[:mid]
+        r = arr[mid:]
+
+        mergeSort(l)
+        mergeSort(r)
+
+        i = j = k = 0
+        while i < len(l) and j < len(r):
+            if l[i] < r[j]:
+                arr[k] = l[i]
+                i += 1
+            else:
+                arr[k] = r[j]
+                j += 1
+            k += 1
+
+        while i < len(l):
+            arr[k] = l[i]
+            i += 1
+            k += 1
+        while j < len(r):
+            arr[k] = r[j]
+            j += 1
+            k += 1
